3e31f0aaf703f7ed9d345855dc1c9582f52c556f
daguilarm <damian.aguilarm@gmail.com>
2021-06-24 18:05:27 +0200

    New elements

---
+++ resources/views/tailwind/sections/new-resource.blade.php
@@ -0,0 +1,10 @@
+<a
+    href="{{ $options['actionCreateUrl'] }} "
+    type="button"
+    class="inline-flex ml-2 mt-3 items-center p-2 shadow-lg rounded-lg text-blue-400 hover:text-white bg-white hover:bg-blue-400 focus:outline-none"
+>
+    <!-- Heroicon name: solid/mail -->
+    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+    </svg>
+</a>
